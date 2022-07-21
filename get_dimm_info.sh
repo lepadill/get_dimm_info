a9c44766d97c3db85793c273a5ceb30d0954876e
lepadill <99999744+lepadill@users.noreply.github.com>
2022-07-21 16:48:04 -0500

    Update get_dimm_info.sh

--- get_dimm_info.sh
+++ get_dimm_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#Parameters
 : "${1?Must provide node name}"
 NODE_ID=$1
-ssh $1 dmidecode -t memory | grep -A 1 Serial > dmidecode.txt
+ssh $1 dmidecode -t memory | grep -B6 Serial > dmidecode.txt
 pwd=$(pwd)
 python $pwd/get_dimm_inventory_info.py
+
