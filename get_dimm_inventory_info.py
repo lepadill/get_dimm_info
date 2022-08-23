2796403daf2bac3f79ba4562131269c0d660f4d7
lepadill <99999744+lepadill@users.noreply.github.com>
2022-08-22 19:39:41 -0500

    Update get_dimm_inventory_info.py

--- get_dimm_inventory_info.py
+++ get_dimm_inventory_info.py
@@ -1,14 +1,35 @@
-from xlrd import open_workbook
+import os, time
 class dimm_inventory:    
     def __init__(self,inventory):
+        proxy = 'http://proxy-us.intel.com:911'
+        try:
+            from xlrd import open_workbook
+        except:
+            with open('libs.sh','w') as bash_file:
+                bash_file.write('export http_proxy='+proxy+'\n'+'export https_proxy='+proxy+'\n'+'pip install xlrd'+'pip install tqdm\n'+'unset http_proxy\nunset https_proxy')
+            os.system('chmod 777 libs.sh \n ./libs.sh')
+            time.sleep(3.5)
+            os.system('rm libs.sh \n clear')
+        from xlrd import open_workbook
         self.book = open_workbook(inventory)
         self.dws = self.book.sheet_by_index(0)
-    def get_dmidecode(self):
+    
+    def check_ssh_connection(self):
+        with open('ssh_test.txt') as f:
+            try:
+                ssh_result = f.readline()
+                ssh_result = str(ssh_result).replace('\n','')
+            except:
+                print('Unable to establish SSH connection...')
+        return ssh_result
+    
+    def get_dmidecode_data(self):
         with open("dmidecode.txt", "r") as file:
             self.dmidecode_info = file.read()
             self.dmidecode_info = self.dmidecode_info.replace('\n','').replace('\t','')
             self.dmidecode_info = self.dmidecode_info.split('--')
         return self.dmidecode_info
+    
     def get_location_serials(self):
         self.location_list = []
         self.serial_list = []
@@ -30,11 +51,13 @@ class dimm_inventory:
         else:
             pass
         return self.location_list, self.serial_list
+    
     def get_inventory_rows(self):
         rows = 0
         for x in self.dws:
             rows += 1
         return rows-1
+    
     def match_info(self,serial,rows):
         row = 0
         while row <= rows:
@@ -51,22 +74,26 @@ class dimm_inventory:
 
 def main():
     dimm_info = dimm_inventory('cmdb_ci_hardware.py')
-    dmidecode_info = dimm_info.get_dmidecode()
-    location_list, serials_list = dimm_info.get_location_serials()
-    full_rows =  dimm_info.get_inventory_rows()
-    with open('dimm inventory.csv','w',encoding = 'utf-8') as file:
-        file.write('Location,Vendor,Model,Serial Number,Barcode,Borrower'+'\n')
-    try:
-        for index, x in enumerate (serials_list):
-            full_dimm_info = dimm_info.match_info(x,full_rows)
-            if ',' in full_dimm_info:
-                full_dimm_info = full_dimm_info.replace(',','')
-            print(location_list[index]+' | '+full_dimm_info)
-            with open('dimm inventory.csv','a',encoding = 'utf-8') as file:
-                full_dimm_info = full_dimm_info.replace('|',',')
-                file.write(location_list[index]+','+full_dimm_info+('\n'))
-    except:
-        pass
+    ssh_test = dimm_info.check_ssh_connection()
+    if ssh_test == '0':
+        dmidecode_info = dimm_info.get_dmidecode_data()
+        location_list, serials_list = dimm_info.get_location_serials()
+        full_rows =  dimm_info.get_inventory_rows()
+        with open('dimm inventory.csv','w',encoding = 'utf-8') as file:
+            file.write('Location,Vendor,Model,Serial Number,Barcode,Borrower'+'\n')
+        try:
+            for index, x in enumerate (serials_list):
+                full_dimm_info = dimm_info.match_info(x,full_rows)
+                if ',' in full_dimm_info:
+                    full_dimm_info = full_dimm_info.replace(',','')
+                print(location_list[index]+' | '+full_dimm_info)
+                with open('dimm inventory.csv','a',encoding = 'utf-8') as file:
+                    full_dimm_info = full_dimm_info.replace('|',',')
+                    file.write(location_list[index]+','+full_dimm_info+('\n'))
+        except:
+            pass
+    else:
+        print('No SSH connection...')
+
 if __name__ == "__main__":
-    main()
-    
+    main() 
