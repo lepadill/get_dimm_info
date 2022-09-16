a3b82cd5009899c30d364ce68dda86ae286f2a3c
lepadill <99999744+lepadill@users.noreply.github.com>
2022-09-16 17:41:23 -0500

    Update get_dimm_info.sh

--- get_dimm_info.sh
+++ get_dimm_info.sh
@@ -3,7 +3,7 @@
 NODE_ID=$1
 pwd=$(pwd)
 ssh -o ConnectTimeout=5 -q $1 exit
-echo $? > ssh_text.txt
+echo $? > ssh_test.txt
 sed -i /$1/d ~/.ssh/known_hosts
 ssh -o ConnectTimeout=5 $1 dmidecode -t memory | grep -B6 Serial > dmidecode.txt
 python $pwd/get_dimm_inventory_info.py
