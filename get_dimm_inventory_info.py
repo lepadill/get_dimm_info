b0d2d17d4d55d02e20fe5a88ab5e151c6829f034
luis.padilla.santoscoy@intel.com <luis.padilla.santoscoy@intel.com>
2023-08-02 21:25:33 -0600

    up

--- get_dimm_inventory_info.py
+++ get_dimm_inventory_info.py
@@ -107,7 +107,7 @@ def main():
                 #with open('dimm inventory.csv','a',encoding = 'utf-8') as file:
                 #    full_dimm_info = full_dimm_info.replace('|',',')
                 #    file.write(location_list[index]+','+full_dimm_info+('\n'))
-            print(tabulate(final_table, headers=headers, tablefmt="fancy_grid"))
+            print(tabulate(final_table, headers=headers, tablefmt="rounded_outline"))
         except:
             pass
     else:
