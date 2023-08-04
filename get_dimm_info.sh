aead4d12e0183fffdcca38078bc4c45826cc3a89
luis.padilla.santoscoy@intel.com <luis.padilla.santoscoy@intel.com>
2023-08-03 20:20:55 -0600

    debug

--- get_dimm_info.sh
+++ get_dimm_info.sh
@@ -5,7 +5,7 @@ pwd=$(pwd)
 ssh -o ConnectTimeout=5 -q $1 exit
 echo $? > ssh_test.txt
 echo $1 > node.txt
-sed -i /$1/d ~/.ssh/known_hosts
+#sed -i /$1/d ~/.ssh/known_hosts
 ssh -o ConnectTimeout=5 $1 dmidecode -t memory | grep -B6 Serial > dmidecode.txt
 if python -c "import tabulate" &> /dev/null; then
     python $pwd/get_dimm_inventory_info.py
