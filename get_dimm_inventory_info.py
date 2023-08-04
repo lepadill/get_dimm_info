4c27a8e7e4ce9a39f873885cd4badcda4efd8832
luis.padilla.santoscoy@intel.com <luis.padilla.santoscoy@intel.com>
2023-08-03 21:24:13 -0600

    debug

--- get_dimm_inventory_info.py
+++ get_dimm_inventory_info.py
@@ -23,7 +23,7 @@ class dimm_inventory:
             node = str(node).replace('\n','')
             #print(node)
             
-            self.dmidecode_info = os.popen(f'ssh -q -o "StrictHostKeyChecking no" {node} ls)# dmidecode -t memory | grep -B6 Serial').read()
+            self.dmidecode_info = os.popen(f'ssh -q -o "StrictHostKeyChecking no" {node} ls')# dmidecode -t memory | grep -B6 Serial').read()
             print(self.dmidecode_info)
             self.dmidecode_info = self.dmidecode_info.replace('\n','').replace('\t','')
             self.dmidecode_info = self.dmidecode_info.split('--')
